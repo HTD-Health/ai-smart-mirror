99be848226be7a4bda62614a549792842b0dc6da
Jedrzej <jedrzej.polaczek@gmail.com>
2021-02-13 18:42:07 +0100

    fix: replace spaces with tabs.

--- src/sensors/sensors_tests/HC-SR04/HC-SR04_simple_test.py
+++ src/sensors/sensors_tests/HC-SR04/HC-SR04_simple_test.py
@@ -12,11 +12,11 @@ def run_hc_sr04(trigger_pin, echo_pin):
 			# "Warm up" trigger pin...
             GPIO.output(trigger_pin, GPIO.LOW)           # Set pin to LOW
             time.sleep(2)                                # Wait 2 seconds
-			
+
             GPIO.output(trigger_pin, GPIO.HIGH)          # Set pin to HIGH
             time.sleep(0.0001)                           # wait 10 microseconds (10us HIGH pulse)
             GPIO.output(trigger_pin, GPIO.LOW)           # Set pin to LOW (End 10us HIGH pulse)
-			
+
 			# Measure distance
             while GPIO.input(echo_pin) == 0:             # wait for input from echo. if LOW record time
                 pulse_start = time.time()
@@ -38,15 +38,13 @@ def run_hc_sr04(trigger_pin, echo_pin):
 
 if __name__ == '__main__':
 	# Creates Argument Parser object named parser
-    parser = argparse.ArgumentParser()
-	
+	parser = argparse.ArgumentParser()
+
 	# Argument 1: Trigger output pin
-    parser.add_argument('--trig', type=int, default=7,
-                        help='PIN with TRIGGER output PIN.')
+	parser.add_argument('--trig', type=int, default=7, help='PIN with TRIGGER output PIN.')
 
     # Argument 2: Echo input pin
-    parser.add_argument('--echo', type=int, default=11,
-                        help='PIN with ECHO input PIN.')
+	parser.add_argument('--echo', type=int, default=11, help='PIN with ECHO input PIN.')
 
 	# Get command line arguments
 	init_args = parser.parse_args()
@@ -54,7 +52,7 @@ if __name__ == '__main__':
 	echo_pin = init_args.echo
 	
 	print('Setting up GPIO...')
-	GPIO.setmode(GPIO.BOARD)			# Setting PIN numbers context. GPIO.BOARD map PIN numbers like on BOARD. GPIO.BCM map PIN numbers like in docummentation.
+	GPIO.setmode(GPIO.BOARD)  # Setting PIN numbers context. GPIO.BOARD map PIN numbers like on BOARD. GPIO.BCM map PIN numbers like in docummentation.
 	GPIO.setup(trigger_pin, GPIO.OUT)
 	GPIO.setup(echo_pin, GPIO.IN)
 						
