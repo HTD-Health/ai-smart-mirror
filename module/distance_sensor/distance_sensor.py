0485a85e134d4a69eb5f63f726e857cc9e90b690
Konrad Nojman <konrad.nojman@htdevelopers.com>
2021-02-21 14:36:18 +0100

    Refactor code to separate functions

--- module/distance_sensor/distance_sensor.py
+++ module/distance_sensor/distance_sensor.py
@@ -1,70 +1,89 @@
 #!/usr/bin/python
 # Import required Python libraries
 import time
+import argparse
+import sys
 
 import RPi.GPIO as GPIO
 
-# Define GPIO to use on Pi
-# @TODO: move pin consts to config file
-GPIO_TRIGGER = 23
-GPIO_ECHO = 24
-
 # @TODO: move these consts to config file
-TRIGGER_PULSE_TIME = 0.00001  # Needs to be 10us pulse to trigger the sensor
 SLEEP_TIME = 0.1
 SENSOR_SETTLE_TIME = 0.3
 TIME_PRECISION = 0.0001  # s - to low value creates problems with spotting objects close to the sensor
 
 SPEED_OF_SOUND = 34300  # cm/s
 HALF_SPEED_OF_SOUND = SPEED_OF_SOUND / 2
+TRIGGER_PULSE_TIME = 0.00001  # Needs to be 10us pulse to trigger the sensor
 
 
-def send_trigger():
-    # Send 10us pulse to trigger
-    GPIO.output(GPIO_TRIGGER, True)
+def send_trigger(trigger_pin):
+    # Send pulse to trigger
+    GPIO.output(trigger_pin, True)
     time.sleep(TRIGGER_PULSE_TIME)
-    GPIO.output(GPIO_TRIGGER, False)
+    GPIO.output(trigger_pin, False)
 
 
-# Use BCM GPIO references instead of physical pin numbers
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-
-# Set pins as output and input
-GPIO.setup(GPIO_TRIGGER, GPIO.OUT)  # Trigger
-GPIO.setup(GPIO_ECHO, GPIO.IN)  # Echo
-
-print("Ultrasonic Measurement")
-# Set trigger to False (Low)
-GPIO.output(GPIO_TRIGGER, False)
-
-# Allow module to settle
-time.sleep(SENSOR_SETTLE_TIME)
-
-
-try:
+def run_ultrasonic_sensor(trigger_pin: int, echo_pin: int):
     while True:
-        send_trigger()
+        send_trigger(trigger_pin)
 
-        while GPIO.input(GPIO_ECHO) == 0:
+        while GPIO.input(echo_pin) == 0:
             time.sleep(TIME_PRECISION)
         echo_start = time.time()
 
-        while GPIO.input(GPIO_ECHO) == 1:
+        while GPIO.input(echo_pin) == 1:
             time.sleep(TIME_PRECISION)
         echo_stop = time.time()
 
         # Calculate pulse length
         pulse_duration = echo_stop - echo_start
-        distance = round(pulse_duration * HALF_SPEED_OF_SOUND, 2)
+        calculated_distance = round(pulse_duration * HALF_SPEED_OF_SOUND, 2)
 
-        print(f"Ultrasonic Measurement - Distance: {distance} cm")
+        return calculated_distance
 
-        time.sleep(SLEEP_TIME)
-except KeyboardInterrupt:
-    print("End by user keyboard interrupt")
-finally:
-    GPIO.cleanup()
 
-# Reset GPIO settings
-GPIO.cleanup()
+def main():
+    # @TODO: Decide how to pass Pins - via console arguments or Consts from the config file.
+    # Creates Argument Parser object named parser
+    parser = argparse.ArgumentParser()
+
+    # Argument 1: Trigger output pin
+    parser.add_argument('--trig', type=int, default=16, help='PIN with TRIGGER output PIN.')
+    # Argument 2: Echo input pin
+    parser.add_argument('--echo', type=int, default=18, help='PIN with ECHO input PIN.')
+
+    # Get command line arguments
+    init_args = parser.parse_args()
+    trigger_pin = init_args.trig
+    echo_pin = init_args.echo
+
+    print("Ultrasonic Measurement. Setting up GPIO...")
+
+    # GPIO.BOARD map PIN numbers like on BOARD.
+    # GPIO.BCM map PIN numbers like in documentation.
+    GPIO.setmode(GPIO.BOARD)  # Setting PIN numbers context.
+    GPIO.setwarnings(False)
+    GPIO.setup(trigger_pin, GPIO.OUT)
+    GPIO.setup(echo_pin, GPIO.IN)
+
+    # Set trigger to False (Low)
+    GPIO.output(trigger_pin, False)
+
+    # Allow module to settle
+    time.sleep(SENSOR_SETTLE_TIME)
+
+    try:
+        while True:
+            distance = run_ultrasonic_sensor(trigger_pin, echo_pin)
+            print(f"Ultrasonic Measurement - Distance: {distance} cm")
+            time.sleep(SLEEP_TIME)
+    except KeyboardInterrupt:
+        print("End by user keyboard interrupt")
+    finally:
+        GPIO.cleanup()
+        sys.exit(0)
+
+
+if __name__ == '__main__':
+    # Run module
+    main()
